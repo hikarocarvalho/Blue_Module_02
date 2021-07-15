d35b77a7ed8e06ad951152fb49ea7eede4e30354
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-07-14 22:42:06 -0300

    change sizes

--- projects/project01/js/script.js
+++ projects/project01/js/script.js
@@ -1,7 +1,7 @@
 function change(value){
 	if(value == "happy" || value == "angry"){
 		document.getElementById("luffy").style.width = "100%"; 
-		document.getElementById("luffy").style.height = "100%"; 
+		document.getElementById("luffy").style.height = "auto"; 
 		document.getElementById("luffy").style.left = "0"; 
 		document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
 		if(value=="angry"){
@@ -12,7 +12,7 @@ function change(value){
 		}
 	}else{
 		document.getElementById("luffy").style.width = "50%"; 
-		document.getElementById("luffy").style.height = "60%"; 
+		document.getElementById("luffy").style.height = "auto%"; 
 		document.getElementById("luffy").style.left = "10%"; 
 		document.getElementById("name").innerHTML = "Monkey D' Luffy "+value;
 		if(value=="idiot"){
@@ -25,4 +25,10 @@ function change(value){
 	document.getElementById("luffy").src="imgs/"+value+".png";
 	console.log("img/"+value+".png");
 	return 0;
+}
+
+function load(){
+	vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+	vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+	console.log(vw +" "+ vh);
 }
